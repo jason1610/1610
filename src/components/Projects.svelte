e65c3907de24c80de3204bbbb894d62782c6e06c
Jason <jasondanielsmiles@gmail.com>
2023-05-09 00:31:54 +0200

    Add a splash background that turns into a header when scrolling down

---
+++ src/components/Projects.svelte
@@ -0,0 +1,104 @@
+<div class="background">
+	<h1>Projects</h1>
+	<p>Here are some of my projects</p>
+	<p>Here are some of my projects</p>
+
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque odit iusto mollitia repellat
+		eius fuga, doloribus sunt amet? Debitis, nobis vitae, ipsa molestiae, libero incidunt
+		accusantium quibusdam ipsam totam molestias ab? Omnis, quisquam dolores. Alias rerum facilis
+		aperiam magnam, tempore culpa sequi mollitia voluptate eius hic et delectus est accusantium
+		quaerat molestias necessitatibus quisquam nesciunt in fuga. Ducimus dolore veritatis, dolor
+		ratione mollitia expedita sed omnis facere laborum placeat quisquam, ullam, inventore
+		consequatur? Officia deleniti eligendi saepe, sed atque rerum consequuntur repudiandae porro
+		cupiditate eos voluptas possimus perferendis aspernatur recusandae repellat officiis impedit,
+		quaerat incidunt ullam odio placeat et debitis excepturi? Quaerat dolorem debitis minus dolorum
+		nulla. Distinctio porro vitae beatae totam quod illum maxime sed ex mollitia consequatur
+		similique obcaecati, saepe atque voluptas fugiat blanditiis! Adipisci consequatur maiores, quia
+		et numquam impedit, odio fugiat sapiente vitae cupiditate quod sit explicabo suscipit molestias
+		reiciendis fugit exercitationem quam sint id tempora cumque. Earum minima harum quod magnam
+		aliquam, rerum quidem labore repellendus porro itaque illum consequatur accusantium similique
+		quas sit dicta doloribus aut? Error molestiae quam voluptatum veritatis quis at rerum ratione
+		cum, id illo praesentium unde sapiente ad culpa repellendus, hic soluta, amet quos sequi tempora
+		itaque quisquam. Aperiam sunt eligendi molestiae a porro iusto, assumenda dignissimos ad
+		distinctio minima veniam tempore repellendus commodi quasi harum maiores explicabo est doloribus
+		eos. Eaque tenetur quisquam nam beatae quos, qui at molestiae accusamus ullam culpa saepe odit
+		earum, atque ratione adipisci dicta perspiciatis a corporis molestias! Blanditiis ratione sequi
+		nihil natus iste repudiandae dicta placeat maxime ad distinctio provident, facilis a
+		dignissimos, quo id excepturi ipsum? Nostrum id reiciendis dignissimos distinctio ad vero
+		repellendus consequatur deserunt pariatur exercitationem autem labore voluptatum facilis
+		mollitia soluta blanditiis, nobis harum a vel adipisci alias? Suscipit eveniet omnis error. Qui
+		cumque assumenda nemo, repellendus consectetur molestias alias odio dolorem nam soluta facilis!
+		Dolore officiis id voluptatum amet totam facilis alias cupiditate quidem consequuntur? Possimus
+		dolores eaque perspiciatis ipsam numquam illum. Repellat velit possimus vero odit adipisci
+		fugiat qui provident, magnam fuga alias in iure temporibus quaerat deleniti culpa ipsa dolore
+		laudantium quos. Asperiores esse nam ullam iusto tempore, voluptate doloremque veritatis
+		possimus eaque cumque veniam ut quae necessitatibus temporibus eveniet aperiam neque officiis
+		voluptates quaerat consequatur aspernatur quibusdam illum. Quia, nulla similique aliquid vitae
+		optio inventore officia animi dolor minus, quasi, hic sunt quaerat? Nulla aspernatur harum hic
+		officiis optio quibusdam provident doloribus autem perspiciatis nihil quos id quis, pariatur
+		voluptate qui aut velit ab et placeat. Dolorem quae pariatur reiciendis hic tempora accusamus
+		temporibus in inventore? Inventore optio earum labore sed, repudiandae, ullam quis quibusdam
+		deleniti nulla laboriosam, minus deserunt consequatur sunt sit excepturi nobis adipisci ipsa et
+		autem asperiores veniam suscipit. Impedit blanditiis autem ratione? Quod dicta, consectetur
+		voluptas velit sit, aspernatur repellat, odio maxime voluptates magnam natus. Consequatur
+		aspernatur doloremque saepe quidem. Perspiciatis error quam ab exercitationem, aspernatur
+		similique minima dolores, mollitia consequuntur ipsum non. Similique maiores ab minima nemo
+		rerum, provident a optio fugit neque iusto minus laboriosam corrupti necessitatibus! Ut harum
+		numquam vitae nesciunt ipsa in. Inventore accusamus id similique? Molestias, modi cupiditate?
+		Numquam dicta, accusamus illum cum culpa, amet voluptate nemo neque ullam hic odit doloremque
+		eaque magnam assumenda aliquid unde aut voluptatibus placeat odio cupiditate totam iusto
+		quibusdam fuga. Ipsum praesentium vel expedita eaque eveniet tempore vero illum nesciunt
+		laboriosam velit repellendus, eos iure doloremque laborum quaerat repellat laudantium incidunt
+		nam excepturi nemo animi, deleniti est. Perspiciatis deserunt ratione recusandae magnam officiis
+		blanditiis dolorem veritatis in libero suscipit provident totam commodi quia voluptatibus
+		laborum officia voluptas a, ad beatae nobis consequuntur. Molestiae pariatur minus error
+		sapiente facilis autem adipisci quo consectetur sunt odio deleniti quas, cum ad voluptate
+		asperiores ducimus quidem nulla blanditiis recusandae voluptates laudantium officia excepturi!
+		Tenetur corrupti sunt eius recusandae. Ut harum ipsa doloremque, rerum assumenda dicta modi, est
+		dolore explicabo nam optio omnis veniam, numquam hic. Dolore eligendi repellat officia sed
+		recusandae voluptatum ipsa perspiciatis perferendis. Quae alias reiciendis dolore sed
+		perferendis, maxime vero? Quisquam optio omnis ea nostrum beatae eligendi odio, iste animi,
+		sapiente minima ullam, magnam quam recusandae enim? Nesciunt, exercitationem! Suscipit,
+		voluptatum fugit, iusto enim molestias quod debitis voluptate itaque provident obcaecati
+		perferendis, nobis earum? Optio aut consequuntur iste quam! Cumque repellendus iure enim
+		consequuntur ipsam doloribus expedita laudantium minima, magnam ad cupiditate veniam veritatis
+		natus ullam dolorum quia? Nostrum aliquid dignissimos, perspiciatis ea ipsa nisi vitae nemo
+		culpa, aspernatur eaque impedit. Tempore laboriosam provident accusantium illum incidunt
+		facilis, magnam reprehenderit eius perspiciatis maiores perferendis eveniet at dicta sit ducimus
+		exercitationem odit similique rem veritatis. Vitae, labore eaque. Libero cumque animi et iste,
+		ducimus sequi odio ab quaerat impedit adipisci, sed dolores provident laboriosam, beatae error
+		ipsa voluptatum. Tempora suscipit amet vitae voluptas eligendi nisi neque doloribus aliquam
+		assumenda incidunt iure doloremque dicta perferendis, quia laudantium beatae! Debitis, et eos
+		voluptate nam laudantium eum? Cupiditate exercitationem, enim placeat optio veniam recusandae
+		eaque adipisci? Rem, voluptatibus tenetur dicta ex, sint ea tempora est quibusdam fugit quos
+		quia. Eaque voluptates magnam facere officiis iure est. Aut, itaque dolore dolorem repudiandae
+		perspiciatis rerum ullam aperiam dolor natus id totam, obcaecati libero impedit nostrum aliquam
+		consectetur repellat, ex asperiores recusandae praesentium quibusdam quos! Quibusdam,
+		aspernatur? Quos, rem id porro voluptatibus quam ipsa consectetur quidem ipsam sunt impedit.
+		Incidunt asperiores magnam ea harum eaque quos adipisci optio quas! Aliquam a magnam
+		consequuntur asperiores beatae doloribus reiciendis minima quidem nihil nulla tempora laudantium
+		fuga, rerum temporibus eaque id dolor autem ipsum optio. Magnam eius expedita enim atque,
+		maiores veritatis laborum esse ad, repellat inventore quae deleniti blanditiis sit! Laboriosam,
+		sint cum? Reiciendis numquam facere natus quia aliquid. Inventore, fuga aperiam libero, veniam
+		numquam odio omnis, tenetur provident corporis porro rem cumque! Ducimus, tempore, quos atque
+		corporis esse labore dolorem natus laborum placeat exercitationem reiciendis explicabo id
+		impedit dolores, officia facere? Autem, ipsum magni? Pariatur doloremque libero ratione cumque
+		voluptas sit quos perspiciatis ut placeat autem, velit vero facere. Veritatis ratione ducimus
+		ad, eaque architecto, placeat ea labore, perspiciatis voluptatem cumque repellendus quidem
+		consectetur non et quisquam debitis fuga.
+	</p>
+</div>
+
+<style>
+	.background {
+		background-color: white;
+		height: 300vh;
+		margin-top: -40px;
+		padding-top: calc(20vh + 70px);
+		padding-top: calc(20vhl + 70px);
+		padding-left: 20px;
+		padding-right: 20px;
+		color: black;
+	}
+</style>
--- src/components/Space.svelte
+++ src/components/Space.svelte
@@ -4,24 +4,9 @@
 	let spaceBackground: HTMLDivElement;
 	let scrollAmount: number = 0;
 	let maxY: number;
+	let snapToHeader: boolean = false;
 
-	// function setImageHeight() {
-	// 	const desiredHeight = window.innerHeight * 1.8;
-	// 	spaceBackground.style.height = `${desiredHeight}px`;
-	// 	maxY = desiredHeight - window.innerHeight * 0.8;
-	// }
-
-	onMount(() => {
-		// setImageHeight();
-		maxY = spaceBackground.clientHeight - window.innerHeight * 0.8;
-		window.addEventListener('wheel', handleScroll, { passive: false });
-		console.log(maxY);
-		return () => {
-			window.removeEventListener('wheel', handleScroll);
-		};
-	});
-
-	function handleScroll(event: WheelEvent) {
+	const handleWheelScroll = (event: WheelEvent) => {
 		if (scrollAmount < maxY || (window.scrollY === 0 && event.deltaY < 0)) {
 			event.preventDefault();
 			scrollAmount += event.deltaY;
@@ -32,19 +17,49 @@
 			}
 			spaceBackground.style.transform = `translateY(-${scrollAmount}px)`;
 		}
-	}
+		handleScroll();
+	};
+
+	const handleScroll = () => {
+		if (window.scrollY >= window.innerHeight * 0.8 - 70) {
+			snapToHeader = true;
+		} else {
+			snapToHeader = false;
+		}
+	};
+
+	onMount(() => {
+		window.addEventListener('wheel', handleWheelScroll, { passive: false });
+		window.addEventListener('scroll', handleScroll, { passive: false });
+		maxY = spaceBackground.clientHeight - window.innerHeight * 0.8;
+		window.addEventListener('resize', () => {
+			maxY = spaceBackground.clientHeight - window.innerHeight * 0.8;
+		});
+		if (window.scrollY > 0) {
+			scrollAmount = maxY;
+			spaceBackground.style.transform = `translateY(-${scrollAmount}px)`;
+		}
+		return () => {
+			window.removeEventListener('wheel', handleWheelScroll);
+			window.removeEventListener('resize', () => {
+				maxY = spaceBackground.clientHeight - window.innerHeight * 0.8;
+			});
+		};
+	});
 </script>
 
-<div class="space-container">
+<div class={`space-container ${snapToHeader ? 'snap-to-header' : ''}`}>
 	<div class="space-background" bind:this={spaceBackground} />
 </div>
 
 <style>
 	.space-container {
-		position: relative;
 		overflow: hidden;
 		height: 80vh;
 		border-radius: 0 0 30px 30px;
+		position: sticky;
+		top: calc(-80vh + 70px);
+		transition: border-radius 0.2s ease;
 	}
 
 	.space-background {
@@ -56,5 +71,9 @@
 		background-position: bottom;
 		will-change: transform;
 		transition: transform 0.2s ease;
+	}
+
+	.snap-to-header {
+		border-radius: 0;
 	}
 </style>
