23115d22f89ef6d9e1bec82062c026c33bf1fc4d
Jason <jasondanielsmiles@gmail.com>
2023-05-26 21:16:22 +0200

    Add learning section

--- src/components/home/ToolKit.svelte
+++ src/components/home/ToolKit.svelte
@@ -8,33 +8,44 @@
 	import Svelte from '../../assets/logos/svelte.png';
 	import Pixijs from '../../assets/logos/pixijs.svg';
 	import Docker from '../../assets/logos/docker.png';
+	import Tauri from '../../assets/logos/tauri.svg';
+	import Rust from '../../assets/logos/rust.png';
 	import Tool from './Tool.svelte';
 </script>
 
 <div class="container">
-	<h2>My Toolkit</h2>
-	<div class="wrapper">
-		<Tool img={Typescript} caption="TypeScript" color="#2d79c7" />
-		<Tool img={React} caption="React" color="#61dbfb" />
-		<Tool img={Svelte} caption="SvelteKit 🤍" color="#ff3c00" />
-		<Tool img={Node} caption="Nodejs" color="#539e43" />
-		<Tool img={MongoDB} caption="MongoDB" color="#13aa52" />
-		<Tool img={GitHub} caption="GitHub" color="#ffffff" />
-		<Tool img={Vite} caption="Vite" color="#758fff" />
-		<Tool img={Docker} caption="Docker" color="#2496ed" />
-		<Tool img={Pixijs} caption="Pixijs" color="#e72264" />
+	<div>
+		<h2>My Toolkit</h2>
+		<div class="wrapper">
+			<Tool img={Typescript} caption="TypeScript" color="#2d79c7" />
+			<Tool img={React} caption="React" color="#61dbfb" />
+			<Tool img={Svelte} caption="SvelteKit 🤍" color="#ff3c00" />
+			<Tool img={Node} caption="Nodejs" color="#539e43" />
+			<Tool img={MongoDB} caption="MongoDB" color="#13aa52" />
+			<Tool img={GitHub} caption="GitHub" color="#ffffff" />
+			<Tool img={Vite} caption="Vite" color="#758fff" />
+			<Tool img={Docker} caption="Docker" color="#2496ed" />
+			<Tool img={Pixijs} caption="Pixijs" color="#e72264" />
+		</div>
+	</div>
+	<div>
+		<h2>Learning...</h2>
+		<div class="wrapper">
+			<Tool img={Tauri} caption="Tauri" color="#ffc131" />
+			<Tool img={Rust} caption="Rust" color="#90522d" />
+		</div>
 	</div>
 </div>
 
 <style>
 	.container {
-		padding-top: 20vh;
 		padding-top: 20lvh;
 		padding-left: 20px;
 		padding-right: 20px;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		gap: 20lvh;
 	}
 
 	h2 {
