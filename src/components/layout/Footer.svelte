32599bbb5a5909fa1783b85cf8155a0e399a1b8a
Jason <jasondanielsmiles@gmail.com>
2023-06-02 10:54:50 +0200

    Add github link

--- src/components/layout/Footer.svelte
+++ src/components/layout/Footer.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import InstagramIcon from '../../assets/icons/instagram.svg';
 	import MailIcon from '../../assets/icons/mail.svg';
+	import GithubIcon from '../../assets/logos/github.png';
 </script>
 
 <div class="container">
 	<div class="instagram">
 		<img src={InstagramIcon} alt="" />
 		<a href="https://www.instagram.com/smiles_dev/" target="_blank">@smiles_dev</a>
+	</div>
+	<div class="github">
+		<img src={GithubIcon} alt="" />
+		<a href="https://github.com/jason1610" target="_blank">@jason1610</a>
 	</div>
 	<div class="mail">
 		<img src={MailIcon} alt="" />
@@ -32,26 +37,19 @@
 		color: var(--text-color-tertiary);
 	}
 
-	.mail {
+	.container > div {
 		display: flex;
 		align-items: center;
 		gap: 5px;
 	}
 
-	.mail img {
+	.container > div > img {
 		width: 20px;
 		aspect-ratio: 1;
 	}
 
-	.instagram {
-		display: flex;
-		align-items: center;
-		gap: 5px;
-	}
-
-	.instagram img {
-		width: 20px;
-		aspect-ratio: 1;
+	.github img {
+		filter: invert(55%) sepia(33%) saturate(1526%) hue-rotate(194deg) brightness(94%) contrast(95%);
 	}
 
 	.copyright {
